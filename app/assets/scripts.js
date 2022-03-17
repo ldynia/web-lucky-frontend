03a9c4b6aaf55ed1a87dee87716ffd4589505e50
ldynia <l_dynowski@yahoo.com>
2022-03-17 13:42:16 +0100

    XHR

--- app/assets/scripts.js
+++ app/assets/scripts.js
@@ -13,9 +13,29 @@ function ready(fn) {
   }
 }
 
+// XHR request
+function ajaxRequest(url, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      callback(xhr.responseText);
+    }
+  };
+  xhr.send();
+}
+
 // document ready
 window.ready(function() {
   document.getElementById('btn-recommend').addEventListener('click', function() {
-    console.log('!!!!!');
-  })
+    ajaxRequest('api/v1/movies/recommend', function(response) {
+      // extract data from response
+      var data = JSON.parse(response);
+      var poster = data[0].poster;
+      var title = data[0].title;
+      // update DOM
+      document.getElementsByTagName('img')[0].src = poster;
+      document.getElementsByTagName('p')[0].textContent = title;
+    });
+  });
 });
